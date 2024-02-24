bc7c4232a1489dd2ff34c2303dd44d690976e85a
Abineth Anantharam <98078409+abinetha@users.noreply.github.com>
2024-02-24 23:33:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  database: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'username',
+    password: 'password',
+    database: 'database'
+  }
+};
